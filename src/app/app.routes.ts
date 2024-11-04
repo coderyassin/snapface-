0c1bb9369abd1437e6ec105eaf48e773a90c8622
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-11-04 11:36:14 +0100

    feat: switch from one route to another

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {FaceSnapListComponent} from './components/face-snap-list/face-snap-list.component';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'facesnaps', component: FaceSnapListComponent },
+];
