765bb9a18d940c189167586158ecbe5eb1a3c8cd
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-11-05 11:29:05 +0100

    feat: switch between Observables with high-level operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {FaceSnapListComponent} from './components/face-snap-list/face-snap-list.component';
 import {HeaderComponent} from './components/header/header.component';
+import {filter, from, interval, map, Observable, tap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  stringList: string[] = ['Hello', 'World', 'Angular'];
+  stringObservable: Observable<string> = from(this.stringList);
+  interval$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.stringObservable.pipe(
+      map(value => value + 'p1'),
+      tap(value => this.logger(value))
+    ).pipe(
+      map(value => value + 'p2'),
+      tap(value => this.logger(value))
+    ).subscribe();
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+  }
 
 }
